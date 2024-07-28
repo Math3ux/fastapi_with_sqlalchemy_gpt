79e4a9ea3b30b0ebb89489b1c4c42fc3b56471cf
Matheus <matheusleidow@gmail.com>
2024-07-28 10:51:00 -0300

    simple get user in db

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {'message': 'Hello World'}
+
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
+    db: Session = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if user == None:
+        return {'message': 'User not found'}
+    db.close();
+    return user
