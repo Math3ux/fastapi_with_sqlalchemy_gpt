94e79667e5186ed0a3a84bfdca5340b983d1c1fd
Matheus <matheusleidow@gmail.com>
2024-07-29 20:05:00 -0300

    created post method in the api

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 from database import SessionLocal
+from sqlalchemy.orm import Session
 from models import User
-from fastapi import FastAPI, HTTPException;
+from fastapi import FastAPI, HTTPException, Depends
+from schemas import UserBase
 
 app = FastAPI()
 
@@ -14,6 +16,16 @@ def get_user(user_id: int):
     db: Session = SessionLocal()
     user = db.query(User).filter(User.id == user_id).first()
     if user is None:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="User not found")
     db.close();
     return user
+
+@app.post('/users/')
+def create_user(user: UserBase):
+    db_user = User(name=user.name, email=user.email, password=user.password)
+    db: Session = SessionLocal()
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    db.close()
+    return db_user
